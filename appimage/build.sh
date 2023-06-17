65513734ff916187dc7d2f6a005752080a4e580d
darealshinji <djcj@gmx.de>
2023-06-17 21:37:54 +0200

    update appimage build files

--- appimage/build.sh
+++ appimage/build.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 set -e
 
-#sudo apt install build-essential autoconf autoconf-archive nasm wget git fuse x11-utils \
-#  libboost-all-dev libsdl2-dev libsdl2-image-dev libsdl2-net-dev libsdl2-ttf-dev \
-#  libzzip-dev zlib1g-dev libpng-dev libvpx-dev libvorbis-dev \
-#  libcurl4-gnutls-dev libminiupnpc-dev libopenal-dev libsndfile1-dev
-
-#libsmpeg-dev libavcodec-dev libavformat-dev libavutil-dev libswscale-dev
+cat <<EOL >/dev/null
+sudo apt install build-essential autoconf autoconf-archive nasm wget git fuse \
+  libboost-all-dev libsdl2-dev libsdl2-image-dev libsdl2-net-dev libsdl2-ttf-dev \
+  libzzip-dev zlib1g-dev libpng-dev libvpx-dev libvorbis-dev \
+  libcurl4-gnutls-dev libminiupnpc-dev libopenal-dev libsndfile1-dev
+EOL
 
 set -x
 
 cd "$(dirname "$0")"
+
+ffmpeg_tag="n6.0"
 
 export LD_LIBRARY_PATH="$PWD/ffmpeg/usr/lib:$LD_LIBRARY_PATH"
 export PKG_CONFIG_PATH="$PWD/ffmpeg/usr/lib/pkgconfig"
@@ -18,7 +20,10 @@ export PKG_CONFIG_PATH="$PWD/ffmpeg/usr/lib/pkgconfig"
 # build FFmpeg from source to avoid unneeded dependencies
 # https://github.com/Aleph-One-Marathon/mac-frameworks/blob/master/ffmpeg/ffmpeg.mk
 if [ ! -d ffmpeg ]; then
-    git clone --depth 5000 https://github.com/FFmpeg/FFmpeg ffmpeg
+    git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg
+    cd ffmpeg
+    git checkout $ffmpeg_tag
+    cd ..
 fi
 if [ ! -f ffmpeg/usr/lib/pkgconfig/libavcodec.pc ]; then
     cd ffmpeg
@@ -97,6 +102,7 @@ if [ ! -f build/Source_Files/alephone ]; then
 fi
 
 # create AppDirs
+rm -rf out
 mkdir -p out/appdir/usr/bin out/appdir/usr/share/doc/{alephone,ffmpeg}
 cp build/Source_Files/alephone out/appdir/usr/bin
 cp ../{AUTHORS,COPYING,THANKS} out/appdir/usr/share/doc/alephone
@@ -105,12 +111,9 @@ cp -r out/appdir out/alephone.appdir
 cp -r out/appdir out/marathon.appdir
 cp -r out/appdir out/marathon2.appdir
 cp -r out/appdir out/marathon-infinity.appdir
-cp -r out/appdir out/marathon-trilogy.appdir
 cp -r ../data/Scenarios/Marathon out/marathon.appdir/usr/share
 cp -r ../data/Scenarios/"Marathon 2" out/marathon2.appdir/usr/share
 cp -r ../data/Scenarios/"Marathon Infinity" out/marathon-infinity.appdir/usr/share
-cp -r ../data/Scenarios out/marathon-trilogy.appdir/usr/share
-cp /usr/bin/xmessage out/marathon-trilogy.appdir/usr/bin
 
 # create AppImages
 cd out
@@ -119,4 +122,3 @@ $deploy -oappimage --appdir=alephone.appdir          -d../alephone.desktop      
 $deploy -oappimage --appdir=marathon.appdir          -d../marathon.desktop          -i../marathon.png          --custom-apprun=../apprun-marathon.sh
 $deploy -oappimage --appdir=marathon2.appdir         -d../marathon2.desktop         -i../marathon2.png         --custom-apprun=../apprun-marathon2.sh
 $deploy -oappimage --appdir=marathon-infinity.appdir -d../marathon-infinity.desktop -i../marathon-infinity.png --custom-apprun=../apprun-marathon-infinity.sh
-$deploy -oappimage --appdir=marathon-trilogy.appdir  -d../marathon-trilogy.desktop  -i../alephone.png          --custom-apprun=../apprun-marathon-trilogy.sh
